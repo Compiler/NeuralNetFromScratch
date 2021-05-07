810fd5fd395b99fad2c6ba15f57a4cf22acc8273
unknown <lukerochedev@gmail.com>
2021-05-06 20:35:35 -0400

    dense layers

--- NP.py
+++ NP.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+class DenseLayer:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases=np.zeros((1,n_neurons))
+
+    def forward(self, inputs):
+        self.output=np.dot(inputs, self.weights)+self.biases
+
 
 inputs= [[1,2,3,2.5],
         [2,5,-1,2],
