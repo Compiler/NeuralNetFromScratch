36469d07940bfbea46846621d8db56584121769d
unknown <lukerochedev@gmail.com>
2021-05-09 10:40:11 -0400

    batched sm

--- NP.py
+++ NP.py
@@ -1,5 +1,8 @@
 import numpy as np
 import math
+import nnfs
+from nnfs.datasets import spiral_data
+nnfs.init()
 class DenseLayer:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
@@ -10,32 +13,30 @@ class DenseLayer:
 
 class Activation_RectifiedLinear:
     def forward(self, inputs):
-        self.outputs=np.maximum(0, inputs)
+        self.output=np.maximum(0, inputs)
 
 class Activation_SoftMax:
-    def activate(self, inputs, outputs):
-        exp_vals = []
-        for output in outputs:
-            exp_vals.append(math.e**output)
-
-        print(exp_vals)
-
-        norm_vals=[]
-        total = sum(exp_vals)
-        for value in exp_vals:
-            norm_vals.append(value / total)
-        print(norm_vals)
+    def forward(self, inputs):
+        exp_vals = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_vals / np.sum(exp_vals, axis=1,keepdims=True)
+        self.output = probabilities
 
 
-inputs= [[1,2,3,2.5],
-        [2,5,-1,2],
-        [-1.5,2.7,3.3,-0.8]]
-weights=[[0.2,0.8,-0.5,1],
-        [0.5, -0.91, 0.26,-0.5],
-        [-0.26,-0.27,0.17,0.87]]
-biases=[2,3,0.5]
-layer_outputs= np.dot(inputs, np.array(weights).T)+biases
-print(layer_outputs)
+data = [[]]
+for i in range(50):
+    for x in range(10):
+        data.append(math.sin(np.random.randn()))
+    #print(data[i])
 
-fn = Activation_SoftMax()
-fn.activate(inputs,layer_outputs[0])
+# x,y = spiral_data(samples=100, classes=3);
+# print(x,'\n',y)
+dense1 = DenseLayer(1, 5)
+act1 = Activation_RectifiedLinear()
+dense2 = DenseLayer(5, 2)
+oact = Activation_SoftMax()
+
+dense1.forward(data)
+act1.forward(dense1.output)
+dense1.forward(act1.output)
+oact.forward(dense2.output)
+print(oact.output)
