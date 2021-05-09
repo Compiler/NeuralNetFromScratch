698092de55f93d47c12dcb4fd1b99a24320982ce
unknown <lukerochedev@gmail.com>
2021-05-09 08:56:01 -0400

    softmax

--- NP.py
+++ NP.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 class DenseLayer:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
@@ -12,6 +12,20 @@ class Activation_RectifiedLinear:
     def forward(self, inputs):
         self.outputs=np.maximum(0, inputs)
 
+class Activation_SoftMax:
+    def activate(self, inputs, outputs):
+        exp_vals = []
+        for output in outputs:
+            exp_vals.append(math.e**output)
+
+        print(exp_vals)
+
+        norm_vals=[]
+        total = sum(exp_vals)
+        for value in exp_vals:
+            norm_vals.append(value / total)
+        print(norm_vals)
+
 
 inputs= [[1,2,3,2.5],
         [2,5,-1,2],
@@ -22,3 +36,6 @@ weights=[[0.2,0.8,-0.5,1],
 biases=[2,3,0.5]
 layer_outputs= np.dot(inputs, np.array(weights).T)+biases
 print(layer_outputs)
+
+fn = Activation_SoftMax()
+fn.activate(inputs,layer_outputs[0])
